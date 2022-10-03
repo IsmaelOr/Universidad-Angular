1a215fbda31e8cb80476ccd46e79e4cac8bc6904
Ismael Ortega Estrada <ismaelortega80@gmail.com>
2022-10-03 09:09:53 -0500

    Sintaxis de Variables

---
+++ JavaScript/Leccion02/02-03 SintaxisdeVariables.js
@@ -0,0 +1,19 @@
+// Sintaxis básica de declaración de variables en JavaScript
+
+var nombre = "Juan"; //"Esta era la forma anterior de declarar variables";
+let apellido = "Perez"; //"Esta es la forma de declarar actualmente una variable";
+const constante = "Ismael"; //"Esta es la forma de declarar una constante, su valor no puede cambiar";
+
+// Declaración Múltiple
+let x, y;
+(x = 10), (y = 20);
+let z = x + y;
+console.log(z);
+
+// Reglas de nombres en variables
+/*
+    - Se utiliza notación de Altas y Bajas o conocida como de Camello
+    - JavaScript es sensible a mayusculas y minusculas, no es lo mismo la varibale nombreCompleto que nombrecompleto
+    - No puede empezar con un número el nombre de una variable.
+    - Una variable no se puede llamar como una variable reservada.
+*/
