ddd5b93eea6e1929f0fa01e4886c442e84898a2c
Ismael Ortega Estrada <ismaelortega80@gmail.com>
2022-10-03 13:42:13 -0500

    Operadores en JavaScript

---
+++ JavaScript/Leccion03/03-01 OperadoresJavaScript.js
@@ -0,0 +1,50 @@
+// Operadores de JavaScript
+
+let a = 3,
+  b = 2;
+
+// Suma
+let z = a + b;
+console.log("Resultado de la suma: " + z);
+
+// Resta
+z = a - b;
+console.log("Resultado de la resta: " + z);
+
+// Multiplicacion
+z = a * b;
+console.log("Resultado de la multiplicacion: " + z);
+
+// Division
+z = a / b;
+console.log("Resultado de la división: " + z);
+
+// Modulo
+z = a % b;
+console.log("Resultado del modulo: " + z);
+
+// Exponente
+z = a ** b;
+console.log("Resultado del exponente 3^2: " + z);
+
+// Incremento
+// Pre-incremento(El operador ++ antes de la variable) [Se incrementa y luego se asigna el valor]
+z = ++a;
+console.log("El valor de a: " + a);
+console.log("El valor de z: " + z);
+
+// Pos-incremento (el operador ++ despues de la variable) [Se asigna el valor primero y luego se incrementa]
+z = b++;
+console.log("El valor de b: " + b);
+console.log("El valor de z: " + z);
+
+// Decremento
+// Pre-decremento  [Se decrementa y luego se asigna el valor]
+z = --a;
+console.log("El valor de a: " + a);
+console.log("El valor de z: " + z);
+
+// Pos-decremento [Se asigna el valor primero y luego se decrementa]
+z = b--;
+console.log("El valor de b: " + b);
+console.log("El valor de z: " + z);
