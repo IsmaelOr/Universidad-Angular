c4f2c56e426ebb8866ab14a94c5c243cf06a31b4
Ismael Ortega Estrada <ismaelortega80@gmail.com>
2022-10-04 08:49:16 -0500

    Estructura Switch

---
+++ JavaScript/Leccion04/04-05 EstructuraSwitch.js
@@ -0,0 +1,22 @@
+// Estructura Switch
+let numero = 4;
+let numeroTexto;
+
+switch (numero) {
+  case 1:
+    numeroTexto = "Uno";
+    break;
+  case 2:
+    numeroTexto = "Dos";
+    break;
+  case 3:
+    numeroTexto = "Tres";
+    break;
+  case 4:
+    numeroTexto = "Cuatro";
+    break;
+  default:
+    numeroTexto = "Sin coincidencia";
+}
+
+console.log(numeroTexto);
---
+++ JavaScript/Leccion04/04-06 EjercicionSwitchEstaciones.js
@@ -0,0 +1,31 @@
+// Decir que estacion del año es, de acuerdo al mes del año
+
+let mes = 12;
+let estacion;
+
+switch (mes) {
+  case 1:
+  case 2:
+  case 12:
+    estacion = "Invierno";
+    break;
+  case 3:
+  case 4:
+  case 5:
+    estacion = "Primavera";
+    break;
+  case 6:
+  case 7:
+  case 8:
+    estacion = "Verano";
+    break;
+  case 9:
+  case 10:
+  case 11:
+    estacion = "Otoño";
+    break;
+  default:
+    estacion = "Opcion incorrecta";
+}
+
+console.log(estacion);
